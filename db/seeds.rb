2318f6c0d225ab295edcecd1517ffe48b9a5d217
PNdunguMaina <ndunguwamaina@gmail.com>
2023-02-23 01:29:46 +0300

    Seed rails backend with external space missions API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'rest-client'
+
+def get_missions
+    baseUrl = "https://api.spacexdata.com/v3/missions"
+    missions = RestClient.get(baseUrl)
+    response = JSON.parse(missions)
+
+    response.each do |mission|
+        Mission.create(title: mission['mission_name'], description: mission['description'], website: mission['website'], manufacturer: mission['manufacturers'])
+    end
+end
+
+get_missions()
