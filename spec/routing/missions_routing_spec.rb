54076d9c4856845df51afcc0b5e6483f77c64f8d
PNdunguMaina <ndunguwamaina@gmail.com>
2023-02-23 10:55:33 +0300

    Write tests using RSpec for Rails

--- spec/routing/missions_routing_spec.rb
+++ spec/routing/missions_routing_spec.rb
@@ -10,29 +10,8 @@ RSpec.describe MissionsController, type: :routing do
       expect(get: "/missions/new").to route_to("missions#new")
     end
 
-    it "routes to #show" do
-      expect(get: "/missions/1").to route_to("missions#show", id: "1")
-    end
-
-    it "routes to #edit" do
-      expect(get: "/missions/1/edit").to route_to("missions#edit", id: "1")
-    end
-
-
     it "routes to #create" do
       expect(post: "/missions").to route_to("missions#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(put: "/missions/1").to route_to("missions#update", id: "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(patch: "/missions/1").to route_to("missions#update", id: "1")
-    end
-
-    it "routes to #destroy" do
-      expect(delete: "/missions/1").to route_to("missions#destroy", id: "1")
     end
   end
 end
